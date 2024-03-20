51c044cef5315bda82f5a66803f789d38ebd4560
borhez <getlink32@gmail.com>
2024-03-20 20:08:49 +0300

    start develop-branch

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "quadratic_equation.h"
+
+int main(void) {
+    printf("Решение уравнения:\n");
+    return 0;
+}
---
+++ quadratic_equation.c
@@ -0,0 +1,7 @@
+
+#include "quadratic_equation.h"
+
+roots solve_equation(float a, float b, float c) {
+    roots ret;
+    return ret;
+}
